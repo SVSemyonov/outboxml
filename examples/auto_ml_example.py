1b74aea14663badb65a9f89e405db64bd328b95d
justsuvorov <justsuvorov@gmail.com>
2025-03-23 20:30:24 +0300

    config read and path to file

--- examples/auto_ml_example.py
+++ examples/auto_ml_example.py
@@ -1,3 +1,4 @@
+import json
 import time
 from typing import Callable
 
@@ -8,7 +9,7 @@ from outboxml.automl_utils import check_for_new_data
 from outboxml.automl_manager import AutoMLConfig
 
 with open('./configs/test_configs/automl-titanic.json', encoding='utf-8') as f:
-    auto_ml_config = AutoMLConfig.model_validate(f.read())
+    auto_ml_config = AutoMLConfig.model_validate(json.load(f))
 
 
 def main(auto_ml_script: Callable = titanic_example, auto_ml_config: AutoMLConfig = auto_ml_config, config=config):
