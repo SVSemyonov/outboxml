8a4d26403c2c206b19f3bb2f705a1027da86c227
justsuvorov <justsuvorov@gmail.com>
2025-08-26 19:07:21 +0300

    service example

--- outboxml/service.py
+++ outboxml/service.py
@@ -1,7 +1,12 @@
+import config
 from outboxml.main_predict import app
 import uvicorn
 
-def main(host="0.0.0.0", port=8000):
+from outboxml.main_release import MLFLowRelease
+
+
+def main(host="0.0.0.0", port=8000, group_name: str = 'example'):
+    MLFLowRelease(config=config).load_model_to_source_from_mlflow(group_name=group_name)
     uvicorn.run(app, host=host, port=port)
 
 if __name__ == "__main__":
