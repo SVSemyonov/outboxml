03fe41dba2627924d5b6b93b1ce32fd1a2087dd8
dima2y5 <dimazorro34@gmail.com>
2026-01-27 23:13:06 +0300

    tests(monitoring): Addition test_monitoring.py

--- tests/test_monitoring.py
+++ tests/test_monitoring.py
@@ -3,7 +3,8 @@ from unittest import TestCase
 import pandas as pd
 
 from outboxml.monitoring_manager import MonitoringManager
-from outboxml.monitoring_result import MonitoringResult
+from outboxml.monitoring_result import MonitoringResult, DataContext
+from outboxml.core.monitoring_factory import DataReviewerRegistry, DataReviewerComponent
 from outboxml.extractors import Extractor
 from outboxml.metrics.base_metrics import BaseMetric
 
@@ -14,6 +15,26 @@ monitoring_config = test_configs_path / 'monitoring_test_config.json'
 
 path_to_data = test_data_path / 'titanic.csv'
 
+@DataReviewerRegistry.register('test_dataset_review')
+class DataSetReview(DataReviewerComponent):
+    def __init__(self):
+        super().__init__()
+
+    def review(self, data_context: DataContext)-> pd.DataFrame:
+        df = data_context.base
+        res = []
+
+        for column in df.columns:
+            missing = df[column].isnull().mean()
+
+            res.append({
+                'column': column,
+                'missing': missing,
+                'X_train_length': len(data_context.X_train_index),
+                'X_test_length': len(data_context.X_test_index),
+            })
+
+        return pd.DataFrame(res)
 
 class LogsExtractor(Extractor):
     def extract_dataset(self) -> pd.DataFrame:
@@ -28,10 +49,11 @@ class TestMonitoringManger(TestCase):
         pass
 
     def test_monitoring(self):
-        review = MonitoringManager(monitoring_config=str(monitoring_config),
+        review_result = MonitoringManager(monitoring_config=str(monitoring_config),
                                    models_config=str(config_name),
                                    business_metric=BusinessMetricsExample(),
                                    logs_extractor=LogsExtractor()
-                                   ).review(send_mail=True, )
-        self.assertIsInstance(review, MonitoringResult)
-        self.assertAlmostEqual(review.reviews['datadrift']['first']['PSI']['SEX'], 0.002, 2)
+                                   ).review(send_mail=False, )
+        self.assertIsInstance(review_result, MonitoringResult)
+        self.assertAlmostEqual(review_result.reviews['datadrift']['first']['PSI']['SEX'], 0.002, 2)
+        self.assertEqual(review_result.reviews['test_dataset_review']['X_train_length'].max(), 712)
