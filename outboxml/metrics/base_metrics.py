875404d99d926c857df0523b9df5fe86f99a0471
MatseraMS <70859966+MatseraMS@users.noreply.github.com>
2026-04-13 15:57:45 +0300

    Fix/gitignore n clf metrics (#68)

--- outboxml/metrics/base_metrics.py
+++ outboxml/metrics/base_metrics.py
@@ -95,7 +95,7 @@ class BaseMetrics(BaseMetric):
                                                                  (y_pred_exp > cutoff).astype(int),
                                                                  sample_weight=self._exposure), 4),
                         'recall_score': round(recall_score(self._y_true,
-                                                           y_pred_exp,
+                                                           (y_pred_exp > cutoff).astype(int),
                                                            sample_weight=self._exposure), 4),
                         'gini': round(2 * roc_auc_score(self._y_true, y_pred_exp,
                                                    sample_weight=self._exposure) - 1, 4)
